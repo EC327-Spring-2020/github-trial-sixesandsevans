5eef19b39f688e562d080360b0148c628fe2f5fb
Evans <evanss@signals29.bu.edu>
2020-02-14 12:23:50 -0500

    Finished the assignment for Lab3 - need to be checked off

---
+++ Lab3/Range.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Start;
+	int Step;
+	int Stop;
+
+	cout << "Enter a Starting Value: " << endl;
+	cin >>  Start;
+	cout << "Enter a Step  Value: " << endl;
+	cin >> Step;
+	cout << "Enter a Stopping  Value: " << endl;
+        cin >> Stop;
+	
+	if (Start > Stop) {
+		for (int count = Start; count >= Stop; count -= Step) {
+			cout << count <<", ";
+		}
+		cout << "" << endl;
+	} 
+
+
+	else  if (Start < Stop) {
+                for (int count = Start ; count <=  Stop; count += Step) {
+                        cout << count <<", ";
+                }
+		cout << "" << endl;
+        }
+	else if (Start == Stop) {
+		cout << "Bye!!" << endl;
+	}
+
+	cout << "Starting: " << Start << " Step: " << Step << " Stopping: " << Stop << endl;
+
+	 return 0;
+	
+}
